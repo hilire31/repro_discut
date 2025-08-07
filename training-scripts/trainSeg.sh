3cd008e782d52398f1dcaf46bc25f172355ce135
firmin <firminrousseau@gmail.com>
2025-08-07 14:46:51 +0200

    fix sbatch

--- training-scripts/trainSeg.sh
+++ training-scripts/trainSeg.sh
@@ -1,9 +1,27 @@
-#!/bin/bash
+
 
 #### This is script to develop model for segmentation task (Task 1) for .tok files 
 #### Get the  base path upto the "data" folder as an argument (Eg: If the data folder is in the path /home/work/disrpt2025/data/ then the basepath_arg is /home/work/disrpt2025 )
 
-basepath_arg="$1"
+#SBATCH --output=logs/launcher-%j.out
+#SBATCH --error=logs/launcher-%j.err
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=L40SNodes
+#SBATCH --gres=gpu:1
+#SBATCH --gres-flags=enforce-binding
+
+export HF_DATASETS_CACHE="/home/froussea/.cache/huggingface/datasets/cache_job_$SLURM_JOB_ID"
+
+nvidia-smi
+# Spécifie l’environnement Python à utiliser
+export ENV=/projects/andiamo/froussea/venv/bin/python
+# Spécifie l'emplacement de la singularité
+export SING=/apps/containerCollections/CUDA12/pytorch2-NGC-24-02.sif
+
+# Crée le dossier de logs si nécessaire
+mkdir -p logs
 
 
 sh prepareTrainSegFiles.sh  $basepath_arg #seg_train.json
