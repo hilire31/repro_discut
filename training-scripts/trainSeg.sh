eac68e920c5df99a68edf6812e83c1520745a00f
firmin <firminrousseau@gmail.com>
2025-08-07 14:49:46 +0200

    fix sbatch

--- training-scripts/trainSeg.sh
+++ training-scripts/trainSeg.sh
@@ -1,8 +1,9 @@
-
+#!/bin/sh
 
 #### This is script to develop model for segmentation task (Task 1) for .tok files 
 #### Get the  base path upto the "data" folder as an argument (Eg: If the data folder is in the path /home/work/disrpt2025/data/ then the basepath_arg is /home/work/disrpt2025 )
 
+#SBATCH --job-name=discutsdisrupt
 #SBATCH --output=logs/launcher-%j.out
 #SBATCH --error=logs/launcher-%j.err
 
