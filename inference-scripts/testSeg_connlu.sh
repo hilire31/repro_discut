10ae4a3af6a76064ba5a282d014a2eb6a7181121
firmin <firminrousseau@gmail.com>
2025-08-08 14:29:35 +0200

    launcher and GPUNodes

--- inference-scripts/testSeg_connlu.sh
+++ inference-scripts/testSeg_connlu.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=discutsdisrupt
-#SBATCH --output=logs/launcher-%j.out
-#SBATCH --error=logs/launcher-%j.err
-
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --partition=L40SNodes
-#SBATCH --gres=gpu:1
-#SBATCH --gres-flags=enforce-binding
-
-export HF_DATASETS_CACHE="/home/froussea/.cache/huggingface/datasets/cache_job_$SLURM_JOB_ID"
-
-nvidia-smi
-# Spécifie l’environnement Python à utiliser
-export ENV=/projects/andiamo/froussea/venv/bin/python
-# Spécifie l'emplacement de la singularité
-export SING=/apps/containerCollections/CUDA12/pytorch2-NGC-24-02.sif
-
-# Crée le dossier de logs si nécessaire
-mkdir -p logs
-
-
 
 
 
